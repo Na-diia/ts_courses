723927ef8ec2c1764fca023b084d28095a7c5c6f
Pekaruk Nadiia <108739131+Na-diia@users.noreply.github.com>
2023-09-22 16:14:23 +0300

    summary

--- dist/concatenation.js
+++ dist/concatenation.js
@@ -34,11 +34,85 @@ function isDog(pet) {
     return 'bark' in pet;
 }
 ;
-if (isDog(pet)) {
-    pet.bark();
+const ceo = {
+    name: 'Alice',
+    id: 2,
+    employees: [{
+            name: 'Petro',
+            id: 5,
+        }],
+};
+let value;
+value = 1;
+value = 2;
+let choise;
+choise = 'yes';
+choise = 'no';
+function getButtonStyle(size) {
+    switch (size) {
+        case 'small':
+            return {
+                fontSize: '14px',
+                padding: '10px',
+            };
+        case 'medium':
+            return {
+                fontSize: '18px',
+                padding: '10px',
+            };
+        case 'large':
+            return {
+                fontSize: '20px',
+                padding: '15px',
+            };
+        default:
+            return {
+                fontSize: '14px',
+                padding: '10px',
+            };
+    }
+    ;
 }
-else {
-    pet.swim();
+;
+let myButtonStyle = getButtonStyle('large');
+const getUserName = (users) => {
+    return users.map((user) => user.name);
+};
+const users = [
+    { id: 1, name: 'Alice' },
+    { id: 2, name: 'Paraska' },
+];
+const results = getUserName(users);
+console.log(results);
+function logMessage(message) {
+    console.log(message);
 }
+;
+const answer = logMessage('Do not stop me now');
+function doSomething(callback) {
+    callback();
+}
+;
+doSomething(() => {
+    console.log('Callback function!');
+});
+function newError(message) {
+    throw new Error(message);
+}
+;
+function infiniteLoop() {
+    while (true) { }
+}
+;
+let myFunc;
+myFunc = (first, second) => {
+    console.log(`first: ${first}, second: ${second}`);
+};
+myFunc('Radio-Ga-Ga', 90);
+function calc(param1, param2, callback) {
+    console.log('Result: ', callback(param1, param2));
+}
+;
+calc(1, 10, (num1, num2) => num1 + num2);
 export { concatenation };
 //# sourceMappingURL=concatenation.js.map
--- src/concatenation.ts
+++ src/concatenation.ts
@@ -45,11 +45,119 @@ function isDog (pet: Dog | Fish): pet is Dog {
   return 'bark' in pet;
 };
 
-if (isDog(pet)) {
-  pet.bark();
-}else {
-  pet.swim();
-}
+type Employee = {
+  name: string;
+  id: number;
+};
 
+type Manager = {
+  employees: Employee[];
+};
 
-export {concatenation}
+type CEO = Employee & Manager;
+
+const ceo : CEO = {
+  name: 'Alice',
+  id: 2,
+  employees: [{
+    name: 'Petro',
+    id: 5,
+  }],
+};
+
+//Literal Type
+type OneOrTwo = 1 | 2;
+let value : OneOrTwo;
+value = 1;
+value = 2;
+
+type YesOrNo = 'yes' | 'no';
+let choise : YesOrNo;
+choise = 'yes';
+choise = 'no';
+
+type ButtonSize = 'small' | 'medium' | 'large';
+
+function getButtonStyle (size: ButtonSize) {
+  switch(size) {
+    case 'small' :
+      return {
+        fontSize: '14px',
+        padding: '10px',
+      };
+      case 'medium' :
+        return {
+          fontSize : '18px',
+          padding: '10px',
+        };
+        case 'large' : 
+        return {
+          fontSize: '20px',
+          padding: '15px',
+        };
+        default: 
+        return {
+          fontSize : '14px',
+          padding: '10px',
+        };
+  };
+};
+
+let myButtonStyle = getButtonStyle('large');
+
+type User = {
+  id: number;
+  name: string;
+};
+
+const getUserName = (users: User[]): string[] => {
+  return users.map((user) => user.name);
+};
+
+const users : User[] = [
+  {id: 1, name: 'Alice'},
+  {id: 2, name: 'Paraska'},
+];
+
+const results = getUserName(users);
+console.log(results);
+
+function logMessage(message: string): void {
+  console.log(message);
+};
+
+const answer = logMessage('Do not stop me now');
+ 
+function doSomething (callback: () => void) {
+  callback();
+};
+
+doSomething(() => {
+  console.log('Callback function!');
+});
+
+function newError (message: string) : never {
+  throw new Error(message);
+};
+
+function infiniteLoop (): never {
+  while(true) {}
+};
+
+let myFunc: (firstArg: string, secondArg: number) => void;
+
+myFunc =(first: string, second: number) => {
+  console.log(`first: ${first}, second: ${second}`);
+};
+
+myFunc('Radio-Ga-Ga', 90);
+
+type CallbackType = (num1: number, num2: number) => number;
+
+function calc (param1: number, param2: number, callback: CallbackType): void {
+  console.log('Result: ', callback(param1, param2));
+};
+
+calc(1, 10, (num1, num2) => num1 + num2);
+
+export {concatenation};
