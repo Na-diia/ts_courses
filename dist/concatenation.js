8ad5b56e96d7bd03caf4639dcf79bb1cab90b39a
Pekaruk Nadiia <108739131+Na-diia@users.noreply.github.com>
2023-09-22 12:53:13 +0300

    summary

--- dist/concatenation.js
+++ dist/concatenation.js
@@ -2,4 +2,43 @@ function concatenation(firstWord, secondWord) {
     console.log(`${firstWord} ${secondWord}`);
 }
 ;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["USER"] = 1] = "USER";
+})(Role || (Role = {}));
+;
+const person = {
+    role: Role.ADMIN,
+};
+if (person.role === Role.ADMIN) {
+    console.log('Role: ', Role.ADMIN);
+}
+;
+let mixedType;
+mixedType = 'ffhj';
+mixedType = 23;
+mixedType = true;
+function combine(param1, param2) {
+    if (typeof param1 === 'number' && typeof param2 === 'number') {
+        return param1 + param2;
+    }
+    else {
+        return param1.toString() + param2.toString();
+    }
+    ;
+}
+;
+let pet;
+function isDog(pet) {
+    return 'bark' in pet;
+}
+;
+if (isDog(pet)) {
+    pet.bark();
+}
+else {
+    pet.swim();
+}
 export { concatenation };
+//# sourceMappingURL=concatenation.js.map
--- src/concatenation.ts
+++ src/concatenation.ts
@@ -3,4 +3,53 @@ function concatenation (firstWord: string, secondWord: string) {
   console.log(`${firstWord} ${secondWord}`);
 };
 
+enum Role {
+  ADMIN,
+  USER,
+};
+
+const person = {
+  role: Role.ADMIN,
+};
+
+if(person.role === Role.ADMIN){
+  console.log('Role: ', Role.ADMIN);
+};
+
+let mixedType : string | number | boolean;
+mixedType = 'ffhj';
+mixedType = 23;
+mixedType = true;
+
+function combine (param1: number | string, param2: number | string) {
+  if(typeof param1 === 'number' && typeof param2 === 'number') {
+    return param1 + param2;
+  } else  {
+    return param1.toString() + param2.toString();
+  };
+};
+
+type Dog = {
+  legs: 4;
+  bark: () => void;
+};
+
+type Fish = {
+  fins: 2;
+  swim: () => void;
+};
+
+let pet : Dog | Fish;
+
+function isDog (pet: Dog | Fish): pet is Dog {
+  return 'bark' in pet;
+};
+
+if (isDog(pet)) {
+  pet.bark();
+}else {
+  pet.swim();
+}
+
+
 export {concatenation}
