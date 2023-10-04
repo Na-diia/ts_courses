742a57e3cc1b41ebdd5af2abbe3f58cca411c7e3
Pekaruk Nadiia <108739131+Na-diia@users.noreply.github.com>
2023-10-04 16:23:44 +0300

    add some class

--- dist/index.js
+++ dist/index.js
@@ -7,4 +7,49 @@ if (button && input) {
     });
 }
 ;
+class Zhiguli_8n {
+    constructor() {
+        this.needRepair = false;
+        this.maxEngineLoad = 5;
+    }
+    checkEngine() {
+        if (this.needRepair) {
+            throw new Error('Engine does not work!!');
+        }
+        ;
+        const engineLoad = Math.floor(Math.random() * this.maxEngineLoad) + 1;
+        if (this.maxEngineLoad === engineLoad) {
+            this.needRepair = true;
+            throw new Error('Engine broken again');
+        }
+        ;
+    }
+    ;
+    startEngine() {
+        this.checkEngine();
+        console.log('Ta-ta-ta-ta');
+    }
+    ;
+    repairEngine() {
+        this.needRepair = false;
+        console.log('Engine rebuilt!');
+    }
+    ;
+}
+;
+const auto = new Zhiguli_8n();
+try {
+    while (true) {
+        auto.startEngine();
+    }
+}
+catch (e) {
+    console.log(e);
+    auto.repairEngine();
+    while (true) {
+        auto.startEngine();
+    }
+    ;
+}
+;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,44 @@ if(button && input) {
         concatenation(input.value, 'hello');
     });
 };
+
+class Zhiguli_8n {
+    private needRepair = false;
+    private maxEngineLoad = 5;
+
+    private checkEngine () {
+        if(this.needRepair) {
+            throw new Error('Engine does not work!!')
+        };
+
+        const engineLoad = Math.floor(Math.random() * this.maxEngineLoad) + 1;
+        if(this.maxEngineLoad === engineLoad) {
+            this.needRepair = true;
+            throw new Error('Engine broken again')
+        };
+    };
+    
+    public startEngine() {
+        this.checkEngine();
+        console.log('Ta-ta-ta-ta');
+    };
+
+    public repairEngine() {
+        this.needRepair = false;
+        console.log('Engine rebuilt!');
+    };  
+};
+
+const auto = new Zhiguli_8n();
+
+try {
+    while (true) {
+        auto.startEngine();
+    }   
+}catch (e){
+ console.log(e);
+ auto.repairEngine();
+ while(true) {
+    auto.startEngine();
+ };
+};
